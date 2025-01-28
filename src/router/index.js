d782ba4203eefae68c417d6547674a90ddcb8b70
HnguynDevGoon <hnguyndevgoon@gmail.com>
2025-01-29 03:15:04 +0700

    Create Banner

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import Login from "../auth/Login.vue";
+import Header from "../components/Header.vue";
 
 const routes = [
   // Redirect '/' to '/Login'
@@ -20,6 +21,11 @@ const routes = [
     path: "/Home",
     name: "Home",
     component: HomeView,
+  },
+  {
+    path: "/header",
+    name: "header",
+    component: Header,
   },
 ];
 
