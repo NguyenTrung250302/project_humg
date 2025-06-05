588894d90385e4867fabbc14ceda52431c9cb006
HnguynDevGoon <hnguyndevgoon@gmail.com>
2025-06-05 18:52:06 +0700

    fix

--- src/admin/EventManager.vue
+++ src/admin/EventManager.vue
@@ -191,6 +191,11 @@
         </div>
       </div>
 
+      <!-- Thông báo thành công -->
+      <div v-if="eventAdminStore.success" class="success-message">
+        {{ eventAdminStore.success }}
+      </div>
+
       <!-- Danh sách sự kiện -->
       <div v-if="eventAdminStore.error" class="error-message">
         {{ eventAdminStore.error }}
@@ -262,6 +267,8 @@ const addEvent = async () => {
   await eventAdminStore.AddNewEvent(newEvent.value, imageFile); // Truyền file ảnh vào hàm
   eventStore.getEventList(); // Cập nhật lại danh sách sự kiện
   showForm.value = false;
+
+  // window.$dialog.success("Thêm sự kiện thành công!");
 
   // Reset form
   newEvent.value = {
@@ -550,6 +557,19 @@ const handleEditImageUpload = (event) => {
 .error-message {
   background: linear-gradient(135deg, #ffebee, #ffcdd2);
   color: #b71c1c;
+  padding: 16px;
+  border-radius: 12px;
+  font-weight: 600;
+  margin: 20px 0;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.05);
+}
+
+.success-message {
+  background: linear-gradient(135deg, #f5f5f5, #adffc6);
+  color: #0cd86b;
   padding: 16px;
   border-radius: 12px;
   font-weight: 600;
